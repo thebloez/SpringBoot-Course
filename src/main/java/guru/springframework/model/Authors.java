c467a09d4c6ecc4fdffe0ae4be9f1cc11557af85
bluscang <ryandunksfamz@gmail.com>
2017-11-18 14:08:16 +0700

    add git ignore

---
+++ src/main/java/guru/springframework/model/Authors.java
@@ -0,0 +1,7 @@
+package guru.springframework.model;
+
+/**
+ * Created by Ryan on 18/11/17.
+ */
+public class Authors {
+}
---
+++ src/main/java/guru/springframework/model/Books.java
@@ -0,0 +1,12 @@
+package guru.springframework.model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Ryan on 18/11/17.
+ */
+@Entity
+public class Books {
+
+
+}
